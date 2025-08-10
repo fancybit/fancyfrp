c6016c172243c39bbfc835d935b6ecfd17185dea
fancybit <fancybit@qq.com>
2025-08-10 16:07:12 +0800

    update

--- frp_0.63.0_linux_arm64/installsrv.sh
+++ frp_0.63.0_linux_arm64/installsrv.sh
@@ -1,12 +1,12 @@
+chmod +x /usr/local/bin/frps
+chmod +x /usr/local/bin/frpsrv.sh
+
 cp frps /usr/local/bin/frps
 # 创建目录（如果不存在）
 mkdir -p /etc/frps/
 cp frps.toml /etc/frps/frps.toml
 cp frpsrv.sh /usr/local/bin/frpsrv.sh
 cp frpsrv.service /etc/systemd/system/frpsrv.service
-
-chmod +x /usr/local/bin/frps
-chmod +x /usr/local/bin/frpsrv.sh
 
 systemctl daemon-reload
 systemctl enable frpsrv
