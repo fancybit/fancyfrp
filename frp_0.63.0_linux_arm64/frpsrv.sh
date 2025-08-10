0032580981c6d178ceb8fc9cadf2cafd9178059b
fancybit <fancybit@qq.com>
2025-08-10 13:40:25 +0800

    update

--- frp_0.63.0_linux_arm64/frpsrv.sh
+++ frp_0.63.0_linux_arm64/frpsrv.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./frps -c ./frps.toml
+/usr/local/bin/frps -c /etc/frps/frps.toml
--- frp_0.63.0_linux_arm64/installsrv.sh
+++ frp_0.63.0_linux_arm64/installsrv.sh
@@ -1,6 +1,11 @@
+cp frps /usr/local/bin/frps
+cp frps.toml /etc/frps/frps.toml
 cp frpsrv.sh /usr/local/bin/frpsrv.sh
-chmod +x /usr/local/bin/frpsrv.sh
 cp frpsrv.service /etc/systemd/system/frpsrv.service
+
+chmod +x /usr/local/bin/frps
+chmod +x /usr/local/bin/frpsrv.sh
+
 systemctl daemon-reload
 systemctl enable frpsrv
 systemctl start frpsrv
